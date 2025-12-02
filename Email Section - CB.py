fe2cd3da917dabd60ed7d0bb58e77c9380d1ca38
Charlie Breckenridge <cbrecken@ramapo.edu>
2025-12-02 01:10:29 -0500

    Update Email Section - CB.py
    Figured out you need to generate an app password also.

--- Email Section - CB.py
+++ Email Section - CB.py
@@ -1,38 +1,55 @@
 # -*- coding: utf-8 -*-
 """
+Created on Tue Dec  2 01:04:12 2025
+
+@author: cpbre
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Wed Nov 26 22:40:22 2025
 
 @author: cpbre
 """
 
-
-import smtplib
+import smtplib       # Import this in to send email at the end
 from email.message import EmailMessage
 
-# Get user email
-user = input("Please enter your email: ")
-print(f"The email you entered is: {user}")
+user = input("Please enter your email: ")   # Prompting user for their email
+print(f"The email you entered is: {user}.")
 
-# Email sending out from
-me = "cbrecken@ramapo.edu"
-print(f"The email sending our message is: {me}") 
 
-# Your email credentials
-email_address = "put your school email here"  # Check that you authorize lower security emails and the like in gmail        
-email_password = "put your password here"
+me = ""                  # The email you will receive mail from
+email_password = ""      # To use this, you need to generate an app password. Put the app password here. 
+# You need to use 2fa. Need to use a gmail account.
 
-# Create the email
+print(f"The email sending our message to you is: {me}. Always check spam folder in case.") 
+
+
+# Gather the information for printing
+#total_results = []
+
+
+
+
+# Create the email formatting
 msg = EmailMessage()
-msg['Subject'] = "Hello from Python!"
-msg['From'] = email_address
+msg['Subject'] = "Prepare for your trip with Team Four's Project!"
+msg['From'] = me
 msg['To'] = user 
-msg.set_content("This is an email sent through Python.")
+msg.set_content("Thank you for using our github project!\n"
+                "Let's print the results!\n")
+#msg.set_content(total_results)
 
-# Send the email
+
 try:
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server: # I keep getting bad credentials errors when trying to send with my correct email and password
-        server.login(email_address, email_password) # The number is 465, not 587
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server: # We are creating a server to send mail through
+        server.login(me, email_password) # The number is 465, not 587, because we are using gmail
         server.send_message(msg)
     print("Email sent successfully!")
 except Exception as e:
     print("Error sending email:", e)
+
+
+
+
